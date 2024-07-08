5d1ff444ae169fce00bd9c15a17752f3b0f6268e
hoanghuy08041977@gmai.com <hoanghuy08041977@gmail.com>
2024-07-08 16:47:35 +0700

    Update Version 1.2

--- src/components/EducationHistory.js
+++ src/components/EducationHistory.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 const EducationHistory = () => {
   return (
-    <div className="my-4">
+    <div className="my-4 container">
       <h3 className="title-in">Education History</h3>
-      <p>Software Engineering of Information Technology, FPT University</p>
-      <p>Year of Graduation: Still studying</p>
-      <ul>
-        <li>Java development</li>
-        <li>Front-end development</li>
-        <li>Mobile programming</li>
+      <p><strong>Software Engineering of Information Technology, FPT University</strong></p>
+      <p><strong>Year of Graduation:</strong> Still studying</p>
+      <ul className="list-group">
+        <li className="list-group-item"><strong>GPA:</strong> 7.0</li>
+        <li className="list-group-item"><strong>Role:</strong> Leader in team for 4 semesters</li>
       </ul>
     </div>
   );
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Skills = () => {
   return (
-    <div className="my-4 title">
+    <div className="my-4 container">
       <h3 className="title-in">Relevant Skills</h3>
-      <ul>
-        <li>Java, Javascript, C++,.NET, NodeJS, Discord.js, HTML</li>
-        <li>Front End & Back End Coding</li>
-        <li>Discord.js</li>
+      <ul className="list-group">
+        <li className="list-group-item">Java, Node.js, C#</li>
+        <li className="list-group-item">HTML, CSS, React.js, jQuery, Ajax</li>
+        <li className="list-group-item">GitHub, GitLab</li>
+        <li className="list-group-item">Front End & Back End Coding</li>
+        <li className="list-group-item">Technical Documentation</li>
       </ul>
     </div>
   );
