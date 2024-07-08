c455026158f6425addc720afa1325ffe75f1a862
hoanghuy08041977@gmai.com <hoanghuy08041977@gmail.com>
2024-07-08 16:16:32 +0700

    Update new version

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const ContactInfo = () => {
   return (
-    <div className="my-4">
-      <h3 className="title-in">Contact Info</h3>
-      <ul className="contact-list">
-        <li>
-          <img className="logo" src="https://i.pinimg.com/564x/ef/dd/14/efdd14aadcfd49b80c78522688ad371f.jpg" alt="Email Icon" />
+    <div className="my-4 container">
+      <h3 className="title-in mb-4">Contact Info</h3>
+      <ul className="list-group">
+        <li className="list-group-item d-flex align-items-center">
+          <img className="logo mr-3" src="https://i.pinimg.com/564x/ef/dd/14/efdd14aadcfd49b80c78522688ad371f.jpg" alt="Email Icon" />
           <a href="mailto:hoanghuy08041977@gmail.com">hoanghuy08041977@gmail.com</a>
         </li>
-        <li>
-          <img className="logo" src="https://static-00.iconduck.com/assets.00/phone-icon-1024x1024-bn4mirqi.png" alt="Phone Icon" />
+        <li className="list-group-item d-flex align-items-center">
+          <img className="logo mr-3" src="https://static-00.iconduck.com/assets.00/phone-icon-1024x1024-bn4mirqi.png" alt="Phone Icon" />
           <a href="tel:+84373702796">+84373702796</a>
         </li>
-        <li>
-          <img className="logo" src="https://t4.ftcdn.net/jpg/06/04/68/99/360_F_604689984_50VpKqlFBCOvSC54HM8Z92uneHoIJ1F9.jpg" alt="LinkedIn Icon" />
+        <li className="list-group-item d-flex align-items-center">
+          <img className="logo mr-3" src="https://t4.ftcdn.net/jpg/06/04/68/99/360_F_604689984_50VpKqlFBCOvSC54HM8Z92uneHoIJ1F9.jpg" alt="LinkedIn Icon" />
           <a href="https://www.linkedin.com/in/huyaty2003/">https://www.linkedin.com/in/huyaty2003/</a>
         </li>
       </ul>
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -2,15 +2,32 @@ import React from 'react';
 
 const WorkExperience = () => {
   return (
-    <div className="my-4">
-      <h3 className="title-in">Work Experience</h3>
-      <p>Programming software, Software engineering with Javascript on Discord community from 2020</p>
-      <ul>
-        <li>Discord.js module for develop bots</li>
-        <li>Modifiers and update discord.js module for community.</li>
-        <li>Music bot, Manage bot,...</li>
-      </ul>
+    <div className="my-4 container">
+      <h3 className="title-in mb-4">Work Experience</h3>
 
+      <div className="timeline-item mb-4">
+        <div className="timeline-content">
+          <h4>Programming software, Software engineering with NodeJS and Discord.js on Discord community.</h4>
+          <p>March 2020 to August 2022</p>
+          <ul>
+            <li>Sold more than 50 bots in Discord community.</li>
+            <li>4.5 stars from buyers.</li>
+            <li>Modifiers and updates for discord.js module for the community.</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="timeline-item">
+        <div className="timeline-content">
+          <h4>Freelancer for coding, support Project</h4>
+          <p>From 2022 to now</p>
+          <ul>
+            <li>Coding web.</li>
+            <li>Coding back-end.</li>
+            <li>Coding SQL.</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
